bfcc6eb26f0124338d7924ece6d69b12b8d60fb9
chahinebeny <chahineben4@gmail.com>
2023-04-16 07:38:28 +0100

    laka

--- js/master.js
+++ js/master.js
@@ -54,6 +54,13 @@ function switchmodele(j){
   let captionTextcolor = document.getElementById("caption-modele");
   captionTextcolor.innerHTML = dots[j].alt;
 }
+switchcolor(0);
+function switchcolor(m){
+    let dots = document.getElementsByClassName("color");
+    let captionTextcolor = document.getElementById("caption-color");
+    captionTextcolor.innerHTML = dots[m].alt;
+  }
+  
 
 var element = document.getElementsByClassName('.sticky-cart-bar');
 if(typeof(element) != 'undefined' && element != null )
